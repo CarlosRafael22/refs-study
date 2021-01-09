571fd9efa937d55d8d0253a378f1b8296eb9b751
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-09 20:55:18 -0300

    feat(simplecallbackref): Add useEffect to log its mouning and unmounting

--- src/cases/SimpleCallbackRef.tsx
+++ src/cases/SimpleCallbackRef.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const SimpleCallbackRef = () => {
-    let inputRef: HTMLInputElement | null;
+    let inputRef: HTMLInputElement;
 
     const onClick = () => {
         console.log('INPUT VALUE: ', inputRef?.value);
@@ -11,12 +11,23 @@ const SimpleCallbackRef = () => {
         console.log('Focus input');
         inputRef?.focus();
     }
+
+    useEffect(() => {
+        console.log('Mounted with inputRef: ', inputRef);
+
+        return () => {
+            console.log('Unmounted with inputRef: ', inputRef)
+        }
+    });
+
     console.log('Rendering')
     return (
         <div>
             <input ref={node => { 
                 console.log('Attaching node: ', node)
-                inputRef = node;
+                if (node) {
+                    inputRef = node;
+                }
              }} />
             <button onClick={onClick}>Log value</button>
             <button onClick={onFocusClick}>Focus on input</button>
