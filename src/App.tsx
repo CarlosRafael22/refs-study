1a3a72ea07f5c83df6dc41143ff8148e1230a8ac
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-09 20:56:00 -0300

    feat(app): Add text between example components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {
   SimpleRef,
   SimpleCallbackRef,
   SimpleCallbackRefRerender,
-  CallbackRef,
+  CallbackRefDynamicChild,
   SimpleForwardRef,
   ImperativeHandle,
   SimpleValueRef
@@ -13,12 +13,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>useRef with instance variable</h3>
       <SimpleValueRef />
+      <h3>useRef with DOM element</h3>
       <SimpleRef />
+      <h3>Callback ref example</h3>
       <SimpleCallbackRef />
+      <h3>Callback ref example with re-render</h3>
       <SimpleCallbackRefRerender />
-      <CallbackRef />
+      <h3>Callback ref example with child added dynamically</h3>
+      <CallbackRefDynamicChild />
+      <h3>Ref forwarding example</h3>
       <SimpleForwardRef />
+      <h3>useImperativeHandle example</h3>
       <ImperativeHandle />
     </div>
   );
